1bf9d8f4d210ec5b9e89ab1a77fe74a191291653
Joe Scott <jbs198585@gmail.com>
2022-10-01 07:22:30 -0400

    Deleted extra headshot image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Headshot from '../img/casual-headshot(1).jpg';
+import Headshot from '../img/casual-headshot.jpg';
 import '../index.css';
 
 const Home = () => {
